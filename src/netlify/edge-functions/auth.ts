a2c943118cfa33a6e415e99f6e18aa60f604a01b
rmartinguerrero <rmartinguerrero@gmail.com>
2026-06-11 22:10:01 +0200

    modificados:     netlify.toml 	modificados:     src/admin/index.html 	nuevos archivos: src/data/fotocv.png 	modificados:     src/data/resume.es.json 	modificados:     src/data/resume.it.json 	modificados:     src/netlify/edge-functions/auth.ts 	nuevos archivos: src/netlify/edge-functions/save.ts 	nuevos archivos: src/netlify/funtions/save.js

--- src/netlify/edge-functions/auth.ts
+++ src/netlify/edge-functions/auth.ts
@@ -4,9 +4,7 @@
 // Le dice a VS Code: "Tranquilo, este archivo usa el entorno de Deno, no te asustes por las URLs"
 // @ts-types="https://edge.netlify.com"
 
-import type { Context } from "https://edge.netlify.com";
-
-export default async (request: Request, context: Context) => {
+export default async (request: Request, context: any) => {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
   
